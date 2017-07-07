2363596cf4769edddc8b7112c685b1c5caf5f409
vadis365 <curve_linear@hotmail.com>
2017-07-07 16:59:23 +0100

    build stuff

--- src/main/java/primal_tech/PrimalTech.java
+++ src/main/java/primal_tech/PrimalTech.java
@@ -26,7 +26,7 @@ import primal_tech.network.FireSticksPacketHandler;
 import primal_tech.proxy.CommonProxy;
 import primal_tech.recipes.ModRecipes;
 
-@Mod(modid = "primal_tech", name = "primal_tech", version = "0.1.24", guiFactory = "primal_tech.configs.ConfigGuiFactory", dependencies = "after:*")
+@Mod(modid = "primal_tech", name = "primal_tech", version = "0.2.0", guiFactory = "primal_tech.configs.ConfigGuiFactory", dependencies = "after:*")
 
 public class PrimalTech {
 
